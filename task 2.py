43d8e3f9eb0e7ef832f635f3f21a53c392891d68
sgshubham728-rgb <sgshubham728@gmail.com>
2025-09-01 15:44:34 +0530

    Add files via upload

--- task 2.py
+++ task 2.py
@@ -1,6 +1,6 @@
 list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 print("Original list:", list_1)
-x = (list_1[0:5])
-print("Extracted first five elements:", x)
-rev = x[::-1]
-print("Reversed extracted elements:", rev)
+list_2=list_1[ :5]
+print("Extracted first five elements:", list_2)
+list_3=list_2[::-1]
+print("Reversed extracted list:",list_3 )
